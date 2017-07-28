749d4c002d26f40076baa80e10b337d5a7702d69
binnala <ydecbin@yahoo.co.jp>
2017-07-28 17:10:56 +0800

    complete

--- src/ios/EmbedWebview.m
+++ src/ios/EmbedWebview.m
@@ -4,25 +4,56 @@
 
 @interface EmbedWebview : CDVPlugin {
   // Member variables go here.
+    UIWebView *webView;
 }
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)open:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation EmbedWebview
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
+- (void)open:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
+    NSString* urlString = [command.arguments objectAtIndex:0];
+    
+    [self showWebViewLoadURL:urlString];
+    
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
-
+-(void)close:(CDVInvokedUrlCommand*)command{
+    CDVPluginResult* pluginResult = nil;
+    NSString* urlString = [command.arguments objectAtIndex:0];
+    
+    [self hideWebView];
+    
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+-(void)showWebViewLoadURL:(NSString*)urlString{
+    if (webView == nil){
+        webView = [[UIWebView alloc] init];
+        CGRect rect = self.viewController.view.frame;
+        rect.origin.y = 108;
+        rect.size.height = rect.size.height - 108;
+        webView.frame = rect;
+        webView.scalesPageToFit = YES;
+        
+        webView.delegate = self.commandDelegate;
+        
+        [self.viewController.view addSubview:webView];
+    }
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];    
+    [webView loadRequest:request];
+    webView.hidden = NO;
+}
+-(void)hideWebView{
+    if (webView != nil){
+        webView.hidden = YES;
+    }
+    
+}
 @end
--- www/EmbedWebview.js
+++ www/EmbedWebview.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "EmbedWebview", "coolMethod", [arg0]);
+exports.open = function(arg0, success, error) {
+    exec(success, error, "EmbedWebview", "open", [arg0]);
+};
+               
+exports.close = function(arg0, success, error) {
+    exec(success, error, "EmbedWebview", "close", [arg0]);
 };
