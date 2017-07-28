5633dc106695b2011c9a578245e3196e937c9e36
binnala <ydecbin@yahoo.co.jp>
2017-07-28 19:36:17 +0800

    fix

--- www/EmbedWebview.js
+++ www/EmbedWebview.js
@@ -4,6 +4,6 @@ exports.open = function(arg0) {
     exec(function(res){}, function(res){}, "EmbedWebview", "open", [arg0]);
 };
                
-exports.close = function(arg0, success, error) {
-    exec(function(res){}, function(res){}, "EmbedWebview", "close", [arg0]);
+exports.close = function() {
+    exec(function(res){}, function(res){}, "EmbedWebview", "close", ['']);
 };
